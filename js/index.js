459f81ca71663f17b8bdf0d9e215a3eff84821f2
BlazerXXI <ruslandendik@gmail.com>
2024-02-13 01:36:14 +0200

    fix: logic

--- js/index.js
+++ js/index.js
@@ -12,11 +12,17 @@ function calculateCapital() {
 	const expenses = parseFloat(expensesInput.value);
 
 	if (!isNaN(salary) && !isNaN(expenses)) {
-		totalResult += salary - expenses;
-		changeHistory(totalResult);
-		updateHistory();
-		salaryInput.value = "";
-		expensesInput.value = "";
+		const newTotalResult = totalResult + salary - expenses;
+
+		if (newTotalResult >= 0) {
+			totalResult = newTotalResult;
+			changeHistory(totalResult);
+			updateHistory();
+			salaryInput.value = "";
+			expensesInput.value = "";
+		} else {
+			alert("Total result cannot be negative. Please enter valid values.");
+		}
 	}
 }
 
@@ -30,9 +36,22 @@ function editResult() {
 }
 
 function updateHistory() {
-	document.getElementById(
-		"totalResult"
-	).innerHTML = `Total Result: <span>${totalResult} &#8372;</span> `;
+	const currentDate = new Date().toLocaleString();
+	const entry = { date: currentDate, value: totalResult };
+
+	const historyData = JSON.parse(localStorage.getItem("historyData")) || [];
+	historyData.push(entry);
+	localStorage.setItem("historyData", JSON.stringify(historyData));
+
+	const totalResultElement = document.getElementById("totalResult");
+	const changeIndicator =
+		totalResult - history !== 0
+			? totalResult > history
+				? `+${totalResult - history} 📈`
+				: `${history - totalResult} 📉`
+			: "";
+
+	totalResultElement.innerHTML = `Total Result: <span>${totalResult}</span> ${changeIndicator}`;
 }
 
 // Handle Enter key press for input fields
